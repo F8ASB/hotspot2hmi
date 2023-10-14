9eb1847a154ba2a9530b27aa856c566668df58a8
F8ASB <f8asb@hotmail.fr>
2023-10-14 15:16:23 +0200

    Update patch.sh

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,1 +1,1 @@
-wget -q http://blog.f8asb.com/wp-content/uploads/2023/10/patch/fonction.patch -O /opt/spotnik/hotpsot2hmi/fonction.test
+wget -q http://blog.f8asb.com/wp-content/uploads/2023/10/patch/fonction.patch -O /opt/spotnik/hotspot2hmi/fonction.test
